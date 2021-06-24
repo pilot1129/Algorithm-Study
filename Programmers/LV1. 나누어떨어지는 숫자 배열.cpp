94c97d4c70bc7c7794db3abe7763a2bc8e84aacf
pilot1129 <pilot1129@naver.com>
2021-06-24 16:53:00 +0900

    LV1 programmers

---
+++ Programmers/LV1. 나누어떨어지는 숫자 배열.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    for(int i = 0; i<arr.size(); ++i)
+    {
+        if(arr[i] % divisor == 0)
+            answer.push_back(arr[i]);
+    }
+    
+    if(answer.empty())
+        answer.push_back(-1);
+    else
+        sort(answer.begin(), answer.end());
+    return answer;
+}
+//https://programmers.co.kr/learn/courses/30/lessons/12910
