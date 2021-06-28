074da899241d9d7d90fda2e286a77a5f1f3a7123
pilot1129 <pilot1129@naver.com>
2021-06-28 15:25:40 +0900

    LV1 programmers

---
+++ Programmers/LV2/LV2. 짝지어 제거하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s) {
+    stack<char> str;
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if (str.empty() || str.top() != s[i])
+            str.push(s[i]);
+        else if (str.top() == s[i])
+            str.pop();
+    }
+    if (str.empty())    
+        return 1;
+    else
+        return 0;
+}
